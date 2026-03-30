f319dec9f95fc242a5992f40562e9935c741b6a0
Nithishnd22 <nithishnd22@gmail.com>
2026-03-30 14:37:37 +0530

    Skills and Contacts sec added

--- src/app/components/contact-component/contact-component.ts
+++ src/app/components/contact-component/contact-component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'contact-component',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact-component.html',
   styleUrl: './contact-component.css',
 })
 export class ContactComponent {
 
+  myForm!: FormGroup
+
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      Name: new FormControl('', []),
+      Email: new FormControl('', []),
+      Message: new FormControl('', [])
+    })
+  }
+
+  get f(){
+    return this.myForm.controls;
+  }
+
+  onSubmit() {
+    console.log('formValues', this.myForm)
+  }
 }
--- src/app/components/project-component/project-component.ts
+++ src/app/components/project-component/project-component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'project-component',
-  imports: [MatTab, MatTabGroup],
+  imports: [MatTab, MatTabGroup, CommonModule],
   templateUrl: './project-component.html',
   styleUrl: './project-component.css',
 })
 export class ProjectComponent {
+
+  projects = [
+    {
+      title: 'ZenBasket  E - Commerce Platform',
+      company: 'Centizen, Inc.',
+      role: 'Software Engineer',
+      duration: 'Feb 2023 - Jun 2025',
+      description: 'A scalable e-commerce platform for handling high-volume transactions.',
+      contributions: [
+        'Designed scalable backend using Node.js, Express.js, and PostgreSQL',
+        'Built microservices architecture for modular services',
+        'Integrated secure payment gateways',
+        'Developed GraphQL APIs for optimized data fetching',
+        'Implemented resolvers and middleware for secure communication',
+        'Configured routing for dynamic queries',
+        'Built reusable Angular UI components',
+        'Worked in Agile environment for feature delivery'
+      ],
+      tech: ['Angular', 'Node.js', 'Express.js', 'PostgreSQL', 'GraphQL']
+    },
+
+    {
+      title: 'Clarus Subsea Integration Platform',
+      company: 'Bizmetric India Pvt Ltd.',
+      role: 'Full Stack Developer',
+      duration: 'Aug 2025 - Feb 2026',
+      description: 'Enterprise SaaS platform for subsea asset integrity management, supporting complex workflows, automation, and large-scale data processing.',
+      contributions: [
+        'Developed full stack features using Angular and Node.js for enterprise SaaS platform',
+        'Built bulk upload module with Excel processing, validation, normalization, and relational mapping',
+        'Implemented advanced data tables with filtering, pagination, full-screen view, and dynamic columns',
+        'Designed and integrated email notification workflows for system and user events',
+        'Developed cron-based schedulers for background jobs and periodic data processing',
+        'Implemented configurable business logic for asset lifecycle and equipment management',
+        'Built dynamic UI workflows for scenario-based property configuration',
+        'Designed REST APIs and Sequelize models for complex relational datasets',
+        'Collaborated with client teams to deliver scalable production features',
+        'Contributed to architecture improvements for better performance and maintainability'
+      ],
+      tech: ['Angular', 'Node.js', 'Express.js', 'Sequelize', 'PostgreSQL', 'Cron Jobs']
+    }
+  ];
 
 }
